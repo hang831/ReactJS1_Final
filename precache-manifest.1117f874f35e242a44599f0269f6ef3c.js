5095e2441b829cd00a33f33365318680b8f2dcc9
hang831 <wuihang831@gmail.com>
2020-07-07 12:41:33 +0800

    Updates

---
+++ precache-manifest.1117f874f35e242a44599f0269f6ef3c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd724309fff4ceb1b38ef23b61ddeaca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b75d3c0f6c754318727d",
+    "url": "/static/css/main.95c89bd8.chunk.css"
+  },
+  {
+    "revision": "2221bd2e32d006371601",
+    "url": "/static/js/2.632cd496.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.632cd496.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b75d3c0f6c754318727d",
+    "url": "/static/js/main.310bd446.chunk.js"
+  },
+  {
+    "revision": "de052b08790a7442c880",
+    "url": "/static/js/runtime-main.a0cc7ce3.js"
+  },
+  {
+    "revision": "d4db85558687d760f7479d9c6e592c1e",
+    "url": "/static/media/bg4.d4db8555.jpeg"
+  },
+  {
+    "revision": "5ac05903cff7fd9e6034b0f9539da51d",
+    "url": "/static/media/inputbar.5ac05903.jpg"
+  }
+]);
